dd7ccc941bd2e450f4eee139e61c440ef1b9e271
gusfurquim <gus@ifsp.edu.br>
2026-04-20 14:35:00 -0300

    Committing sample-app files.

---
+++ sample-app.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p tempdir/templates
+mkdir tempdir/static
+
+cp sample_app.py tempdir/
+cp -r templates/* tempdir/templates/
+cp -r static/* tempdir/static/
+cp Dockerfile_template tempdir/Dockerfile
+
+cd tempdir
+sudo docker build -t sampleapp .
+sudo docker run -t -d -p 8080:8080 --name samplerunning sampleapp

d7a27345aa94badb9da89783d962b3f3368f8e5a
gusfurquim <gus@ifsp.edu.br>
2026-04-20 16:22:25 -0300

    Changed script, no sudo for root

--- sample-app.sh
+++ sample-app.sh
@@ -9,5 +9,9 @@ cp -r static/* tempdir/static/
 cp Dockerfile_template tempdir/Dockerfile
 
 cd tempdir
+#sudo docker build -t sampleapp .
+#sudo docker run -t -d -p 5050:5050 --name samplerunning sampleapp
+
+# No sudo for DevAsc example
 sudo docker build -t sampleapp .
 sudo docker run -t -d -p 5050:5050 --name samplerunning sampleapp
